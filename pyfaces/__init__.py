eff458480302c574a7e584c00afcc5ec189bd9e2
Milad <milad.fa7@hotmail.com>
2025-08-04 00:30:11 +0330

    Update the framework name :)

--- pyfaces/__init__.py
+++
@@ -1,7 +0,0 @@
-from pyfaces.modules.recognition import FaceRecognition
-from .modules.detection import FaceDetection
-from .modules.embedding import FaceEmbedder
-
-
-__all__ = ["FaceDetection", "FaceEmbedder", "FaceRecognition"]
-
---
+++ visionface/__init__.py
@@ -0,0 +1,8 @@
+from .modules.recognition import FaceRecognition
+from .modules.detection import FaceDetection
+from .modules.embedding import FaceEmbedder
+from .modules.landmarks import LandmarkDetection
+
+
+__all__ = ["FaceDetection", "FaceEmbedder", "FaceRecognition", "LandmarkDetection"]
+
