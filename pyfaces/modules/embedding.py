d83547c7206847e04ccf0538741a0c42917d80f9
Milad <milad.fa7@hotmail.com>
2025-08-03 12:45:39 +0330

    Add FaceEmbedder class

--- pyfaces/modules/embedding.py
+++ pyfaces/modules/embedding.py
@@ -1,27 +1,67 @@
-from typing import List, Union
+from typing import Any, List, Union
 import numpy as np
 
+from pyfaces.models.Detector import DetectedFace
 from pyfaces.models.FaceEmbedding import FaceEmbedding
 from pyfaces.modules.modeling import build_model
-from pyfaces.commons.image_utils import load_images
+from pyfaces.commons.image_utils import load_images, validate_images
 
-def embed_faces(
-        face_imgs: Union[str, np.ndarray, List[np.ndarray], List[str]],
-        model_name: str = "FaceNet-VGG",
-        normalize_embeddings: bool = True
-) -> FaceEmbedding:
+
+class FaceEmbedder:
+    """
+    A class for generating embeddings from face images
+    using a specified face embedding model.
+    """
+    def __init__(self, model_name: str = "FaceNet-VGG") -> None:
         """
-        Compute face embeddings for one or more face images.
+        Initializes the FaceEmbedder with the given embedding model.
 
-        Args:
-                face_imgs: A single face image or a list of images as file paths (str) or numpy arrays.
-                model_name: Name of the embedding model to use.
-                normalize_embeddings: Whether to apply L2 normalization to embeddings.
+        Parameters
+        ----------
+        model_name : str, optional
+            The name of the face embedding model to use. Default is "FaceNet-VGG".
+        """
+        self.face_embedder = self.build_model(model_name)
 
-        Returns:
-                FaceEmbedding: Embedding objects for each face.
+    def build_model(self, model_name) -> Any:
+        """
+        Builds and returns the face embedding model.
+
+        Parameters
+        ----------
+        model_name : str
+            The name of the model to load.
+
+        Returns
+        -------
+        Any
+            An initialized face embedding model instance.
+        """
+        return build_model(model_name, "face_embedding")
+    
+    def embed_faces(
+        self,
+        face_imgs: Union[str, np.ndarray, List[np.ndarray], List[str], List[DetectedFace]],
+        normalize_embeddings: bool = True
+    ) -> FaceEmbedding:
+        """
+        Computes face embeddings for one or more face images.
+
+        Parameters
+        ----------
+        face_imgs : Union[str, np.ndarray, List[np.ndarray], List[str], List[DetectedFace]]
+            A single face image or a list of face images. Each image can be a file path (str),
+            a NumPy array, or a DetectedFace object.
+
+        normalize_embeddings : bool, optional
+            Whether to apply L2 normalization to the output embeddings. Default is True.
+
+        Returns
+        -------
+        FaceEmbedding
+            An object containing embedding vectors for each face.
         """
         face_images = load_images(face_imgs)
-        face_embedder = build_model(model_name, "face_embedding")
-        embeded_faces = face_embedder.embed(face_images, normalize_embeddings)
-        return embeded_faces
+        validated_images = validate_images(face_images)
+        return self.face_embedder.embed(validated_images, normalize_embeddings)
+
