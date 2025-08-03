f289adb02b1d96571ea206c719c922a46d736da5
Milad <milad.fa7@hotmail.com>
2025-08-03 21:18:39 +0330

    Add __init__.py to expose main modules

---
+++ pyfaces/__init__.py
@@ -0,0 +1,7 @@
+from pyfaces.modules.recognition import FaceRecognition
+from .modules.detection import FaceDetection
+from .modules.embedding import FaceEmbedder
+
+
+__all__ = ["FaceDetection", "FaceEmbedder", "FaceRecognition"]
+
